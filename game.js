2e72e2ac45fc62f957ea0591810e4b8036bb41b6
Parag Zaveri <parag.g.zaveri@gmail.com>
2018-09-24 09:23:24 -0500

    presentation grade

--- game.js
+++ game.js
@@ -1,77 +1,166 @@
 //game instantiation object
-let config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 0 },
-      debug: false,
-    },
-  },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update,
-  },
-};
+let game = new Phaser.Game(1000, 600, Phaser.AUTO, 'arcade', {
+  preload: preload,
+  create: create,
+  update: update,
+  render: render,
+});
 
 // global game variables
-let sky;
+
 let hero;
 let cursors;
-let gameOver;
-// var player;
-// var stars;
-// var bombs;
-// var platforms;
-// var cursors;
-// var score = 0;
-// var gameOver = false;
-// var scoreText;
+let gameOver = false;
+let heroBullets;
+let heroFireRate = 100;
+let nextFire = 0;
 
+let drones;
+let droneBullets;
+let totalDrones = 0;
+let remainingDrones = 0;
+
+let score = 0;
+let scoreText;
+
+const EnemyDrone = function(name, game, hero, bullets, drone) {
+  let x = game.world.randomX;
+  let y = game.world.randomY;
+  this.name = name;
+  this.game = game;
+  this.health = 2;
+  this.hero = hero;
+  this.droneBullets = bullets;
+  this.enemyFireRate = 1000;
+  this.nextFire = 1000;
+  this.alive = true;
+
+  this.drone = game.add.sprite(x, y, drone);
+  this.drone.anchor.set(0.5);
+  this.drone.scale.setTo(0.25, 0.25);
+
+  game.physics.enable(this.drone, Phaser.Physics.ARCADE);
+};
+
+EnemyDrone.prototype.damage = function() {
+  this.health -= 1;
+  if (this.health <= 0) {
+    this.alive = false;
+    this.drone.kill();
+    // this.drone.disableBody(true, true);
+    return true;
+  }
+  return false;
+};
+
+EnemyDrone.prototype.update = function() {
+  if (this.game.physics.arcade.distanceBetween(this.drone, this.hero) < 400) {
+    if (
+      this.game.time.now > this.nextFire &&
+      this.droneBullets.countDead() > 0 &&
+      this.drone.alive
+    ) {
+      this.nextFire = this.game.time.now + this.enemyFireRate;
+      let bullet = this.droneBullets.getFirstDead();
+      bullet.reset(this.drone.x, this.drone.y);
+      bullet.scale.setTo(0.25, 0.25);
+      bullet.rotation = this.game.physics.arcade.moveToObject(
+        bullet,
+        hero,
+        500
+      );
+    }
+  }
+};
 //create instance of game
-let game = new Phaser.Game(config);
 
 function preload() {
   this.load.image('background', 'assets/background.png');
   this.load.image('heroShip', 'assets/ships/shipgrey3.png');
+  this.load.image('heroBullet', 'assets/bullets/BlueBlast001.png');
+  this.load.image('droneBullet', 'assets/bullets/OrangeBlast__001.png');
 
-  // this.load.image('ground', 'assets/platform.png');
-  // this.load.image('star', 'assets/star.png');
-  // this.load.image('bomb', 'assets/bomb.png');
-  // this.load.spritesheet('dude', 'assets/dude.png', {
-  //   frameWidth: 32,
-  //   frameHeight: 48,
-  // });
+  this.load.image('bluedrone', 'assets/drones/bluedrone1.png');
+  this.load.image('greendone', 'assets/drones/greendrone.png');
+  this.load.image('greydrone', 'assets/drones/greydrone1.png');
+  this.load.image('purpledrone1.png', 'assets/drones/purpledrone1.png');
+  this.load.image('reddrone1.png', 'assets/drones/reddrone1.png');
 }
 
 function create() {
+  game.physics.startSystem(Phaser.Physics.ARCADE);
   //  A simple background for our game
-  sky = this.add.image(400, 300, 'background');
-  // sky.fixedToCamera = true;
+  // game.add.tileSprite(0, 0, 800, 600, 'background');
+  game.stage.backgroundColor = '#000000';
+  // game.world.setBounds(0, 0, 2000, 2000);
+  game.world.resize(6000, 600);
+
   // The hero ship
-  hero = this.physics.add.sprite(200, 100, 'heroShip').setScale(0.25);
-  hero.setCollideWorldBounds(true);
+  hero = game.add.sprite(400, 300, 'heroShip');
+  game.physics.enable(hero, Phaser.Physics.ARCADE);
+  hero.anchor.setTo(0.5, 0.5);
+  hero.scale.setTo(0.25, 0.25);
+  game.camera.follow(hero);
+  hero.body.collideWorldBounds = true;
+  // hero.fixedToCamera = true;
 
   //input events
-  cursors = this.input.keyboard.createCursorKeys();
-  this.cameras.main.startFollow(hero);
+  cursors = game.input.keyboard.createCursorKeys();
+
+  //hero bullets
+  heroBullets = game.add.group();
+  heroBullets.enableBody = true;
+  heroBullets.physicsBodyType = Phaser.Physics.ARCADE;
+  heroBullets.createMultiple(10000, 'heroBullet');
+
+  //enemy bullets
+  droneBullets = game.add.group();
+  droneBullets.enableBody = true;
+  droneBullets.physicsBodyType = Phaser.Physics.ARCADE;
+  droneBullets.createMultiple(25, 'droneBullet');
+
+  //create some enemies
+  drones = [];
+  totalDrones = 30;
+  remainingDrones = 30;
+  for (let i = 0; i < totalDrones; i++) {
+    drones.push(new EnemyDrone(i, game, hero, droneBullets, 'bluedrone'));
+  }
 }
 
 function update() {
   if (gameOver) {
     return;
   }
+  game.physics.arcade.overlap(droneBullets, hero, dronePlayerHit, null, this);
+  remainingDrones = 0;
+  for (let i = 0; i < drones.length; i++) {
+    remainingDrones++;
+    game.physics.arcade.collide(hero, drones[i].drone);
+    game.physics.arcade.overlap(
+      heroBullets,
+      drones[i].drone,
+      playerDroneHit,
+      null,
+      this
+    );
+    drones[i].update();
+  }
   if (cursors.left.isDown) {
-    hero.setVelocityX(-140);
+    hero.body.velocity.x = -200;
+    // game.camera.x -= 4;
   } else if (cursors.right.isDown) {
-    hero.setVelocityX(140);
+    hero.body.velocity.x = 200;
+    // game.camera.x += 4;
   } else if (cursors.up.isDown) {
-    hero.setVelocityY(-80);
+    hero.body.velocity.y = -100;
+    // game.camera.y -= 4;
   } else if (cursors.down.isDown) {
-    hero.setVelocityY(80);
+    hero.body.velocity.y = 100;
+    // game.camera.y += 4;
+  } else {
+    hero.body.velocity.x = 0;
+    hero.body.velocity.y = 0;
   }
   if (cursors.right.isDown && cursors.up.isDown) {
     hero.angle -= 0.4;
@@ -82,140 +171,33 @@ function update() {
   } else if (cursors.left.isDown && cursors.down.isDown) {
     hero.angle += 0.4;
   }
+  if (game.input.activePointer.isDown) {
+    shoot();
+  }
 }
-//  The platforms group contains the ground and the 2 ledges we can jump on
-// platforms = this.physics.add.staticGroup();
 
-//  Here we create the ground.
-//  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-// platforms
-//   .create(400, 568, 'ground')
-//   .setScale(2)
-//   .refreshBody();
+function render() {
+  game.debug.text('Enemies: ' + remainingDrones + ' / ' + totalDrones, 32, 32);
+  game.debug.text('Score: ' + score, 700, 32);
+}
 
-// //  Now let's create some ledges
-// platforms.create(600, 400, 'ground');
-// platforms.create(50, 250, 'ground');
-// platforms.create(750, 220, 'ground');
+function shoot() {
+  if (game.time.now > nextFire && heroBullets.countDead() > 0) {
+    nextFire = game.time.now + heroFireRate;
+    let bullet = heroBullets.getFirstDead();
+    bullet.reset(hero.x, hero.y);
+    bullet.scale.setTo(0.25, 0.25);
+    game.physics.arcade.moveToPointer(bullet, 300);
+  }
+}
 
-// // The player and its settings
-// player = this.physics.add.sprite(100, 450, 'dude');
+function dronePlayerHit(hero, droneBullet) {
+  droneBullet.kill();
+}
 
-// //  Player physics properties. Give the little guy a slight bounce.
-// player.setBounce(0.2);
-// player.setCollideWorldBounds(true);
-
-// //  Our player animations, turning, walking left and walking right.
-// this.anims.create({
-//   key: 'left',
-//   frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-//   frameRate: 10,
-//   repeat: -1,
-// });
-
-// this.anims.create({
-//   key: 'turn',
-//   frames: [{ key: 'dude', frame: 4 }],
-//   frameRate: 20,
-// });
-
-// this.anims.create({
-//   key: 'right',
-//   frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-//   frameRate: 10,
-//   repeat: -1,
-// });
-
-//   //  Input Events
-//   cursors = this.input.keyboard.createCursorKeys();
-
-//   //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-//   stars = this.physics.add.group({
-//     key: 'star',
-//     repeat: 11,
-//     setXY: { x: 12, y: 0, stepX: 70 },
-//   });
-
-//   stars.children.iterate(function(child) {
-//     //  Give each star a slightly different bounce
-//     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-//   });
-
-//   bombs = this.physics.add.group();
-
-//   //  The score
-//   scoreText = this.add.text(16, 16, 'score: 0', {
-//     fontSize: '32px',
-//     fill: '#000',
-//   });
-
-//   //  Collide the player and the stars with the platforms
-//   this.physics.add.collider(player, platforms);
-//   this.physics.add.collider(stars, platforms);
-//   this.physics.add.collider(bombs, platforms);
-
-//   //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-//   this.physics.add.overlap(player, stars, collectStar, null, this);
-
-//   this.physics.add.collider(player, bombs, hitBomb, null, this);
-// }
-
-// function update() {
-//   if (gameOver) {
-//     return;
-//   }
-
-//   if (cursors.left.isDown) {
-//     player.setVelocityX(-160);
-
-//     player.anims.play('left', true);
-//   } else if (cursors.right.isDown) {
-//     player.setVelocityX(160);
-
-//     player.anims.play('right', true);
-//   } else {
-//     player.setVelocityX(0);
-
-//     player.anims.play('turn');
-//   }
-
-//   if (cursors.up.isDown && player.body.touching.down) {
-//     player.setVelocityY(-330);
-//   }
-// }
-
-// function collectStar(player, star) {
-//   star.disableBody(true, true);
-
-//   //  Add and update the score
-//   score += 10;
-//   scoreText.setText('Score: ' + score);
-
-//   if (stars.countActive(true) === 0) {
-//     //  A new batch of stars to collect
-//     stars.children.iterate(function(child) {
-//       child.enableBody(true, child.x, 0, true, true);
-//     });
-
-//     var x =
-//       player.x < 400
-//         ? Phaser.Math.Between(400, 800)
-//         : Phaser.Math.Between(0, 400);
-
-//     var bomb = bombs.create(x, 16, 'bomb');
-//     bomb.setBounce(1);
-//     bomb.setCollideWorldBounds(true);
-//     bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-//     bomb.allowGravity = false;
-//   }
-// }
-
-// function hitBomb(player, bomb) {
-//   this.physics.pause();
-
-//   player.setTint(0xff0000);
-
-//   player.anims.play('turn');
-
-//   gameOver = true;
-// }
+function playerDroneHit(drone, heroBullet) {
+  heroBullet.kill();
+  drone.kill();
+  score += 10;
+  // drones[heroBullet.name].damage();
+}
